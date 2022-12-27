44c7f10bc15fa7ccfdd36082202dd87b24deee0e
sylkos <sylkos49@gmail.com>
2022-12-26 21:20:07 -0500

    added exec command

--- src/lib.rs
+++ src/lib.rs
@@ -48,10 +48,35 @@ impl Command for Stop {
     }
 }
 
+struct Exec {
+    name: String,
+    id: String,
+    cmd: Vec<String>,
+}
+
+#[async_trait]
+impl Command for Exec {
+    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
+        if args.len() < 4 {
+            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
+        }
+        Ok(Box::new(Exec { name: args[1].clone(), id: args[2].clone(), cmd: args[3..].to_vec() }))
+    }
+
+    async fn execute(&self) {
+        let body = format!("{{\"args\":[{}]}}",self.cmd.iter().fold(String::new(), |a, b| format!("{a} \"{b}\",") ).trim().trim_end_matches(","));
+        println!("exec {body}");
+        reqwest::Client::new().post(format!("{ADDR}{}/{}", self.name, self.id))
+            .body(body)
+            .send().await.expect("Failed to send command to the server");
+    }
+}
+
 pub fn match_command(args: Vec<String>) -> Option<Result<Box<dyn Command>, String>> {
     match args[1].as_str() {
         "start" => Some(Start::build_from_args(args)),
         "stop" => Some(Stop::build_from_args(args)),
+        "exec" => Some(Exec::build_from_args(args)),
         _ => None,
     }
 }
