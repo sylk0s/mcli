f1468344f0386cb9a167d79be9e2e033b7bf0925
Sylkos <sylkos49@gmail.com>
2023-09-29 20:52:14 -0400

    rewrote with clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,104 +1,266 @@
-use async_trait::async_trait;
+use clap::{Parser, Subcommand};
 use futures::StreamExt;
-use serde::{Serialize,Deserialize};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 const ADDR: &str = "http://localhost:7955";
 
-#[async_trait]
-pub trait Command {
-    async fn execute(&self);
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> where Self: Sized;
-}
-struct Start {
-    name: String,
-    id: String,
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Optional name to operate on
+    pub name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    pub config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    pub debug: u8,
+
+    #[command(subcommand)]
+    pub command: Option<Commands>,
 }
 
-#[async_trait]
-impl Command for Start {
-    // Maybe someday implement some way to determine which arg is missing?
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 3 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
+#[derive(Subcommand)]
+pub enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    Start {
+        /// The name of the server to start
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to start
+        #[arg(short, long)]
+        id: String,
+    },
+    Stop {
+        /// The name of the server to stop
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to stop
+        #[arg(short, long)]
+        id: String,
+    },
+    Exec {
+        /// The name of the server to execute on
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to execute on
+        #[arg(short, long)]
+        id: String,
+
+        /// The command to execute
+        #[arg(short, long)]
+        cmd: Vec<String>,
+    },
+    Output {
+        /// The name of the server to get output from
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to get output from
+        #[arg(short, long)]
+        id: String,
+    },
+    Status {
+        /// The name of the server to get status from
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to get status from
+        #[arg(short, long)]
+        id: String,
+    },
+    New {
+        /// The name of the server to create
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to create
+        #[arg(short, long)]
+        id: String,
+
+        /// The path to the server files
+        #[arg(short, long)]
+        path: Option<String>,
+
+        /// The port to run the server on
+        #[arg(short, long)]
+        port: Option<u16>,
+
+        /// The version of the server to run
+        #[arg(short, long)]
+        version: Option<String>,
+
+        /// The type of server to run
+        #[arg(short, long)]
+        server_type: Option<String>,
+    },
+    List {
+        /// The name of the server to list
+        #[arg(short, long)]
+        name: String,
+    },
+    CleanOutput {
+        /// The name of the server to get output from
+        #[arg(short, long)]
+        name: String,
+
+        /// The id of the server to get output from
+        #[arg(short, long)]
+        id: String,
+    },
+}
+
+impl Commands {
+    pub async fn execute(&self) {
+        match self {
+            Commands::Test { list } => {
+                if *list {
+                    println!("Listing test values");
+                } else {
+                    println!("Testing");
+                }
+            }
+            Commands::Start { name, id } => {
+                println!("Starting {} {}", name, id);
+                println!(
+                    "{:?}",
+                    reqwest::Client::new()
+                        .put(format!("{ADDR}/{}/{}", name, id))
+                        .send()
+                        .await
+                        .unwrap()
+                );
+            }
+            Commands::Stop { name, id } => {
+                println!("Stopping {} {}", name, id);
+                println!(
+                    "{:?}",
+                    reqwest::Client::new()
+                        .put(format!("{ADDR}/{}/{}", name, id))
+                        .send()
+                        .await
+                        .unwrap()
+                );
+            }
+            Commands::Exec { name, id, cmd } => {
+                let body = format!(
+                    "{{\"args\":[{}]}}",
+                    cmd.iter()
+                        .fold(String::new(), |a, b| format!("{a} \"{b}\","))
+                        .trim()
+                        .trim_end_matches(",")
+                );
+                println!("exec {body}");
+                println!(
+                    "{:?}",
+                    reqwest::Client::new()
+                        .post(format!("{ADDR}/{}/{}", name, id))
+                        .body(body)
+                        .send()
+                        .await
+                );
+            }
+            Commands::Output { name, id } => {
+                let mut stream = reqwest::get(format!("{ADDR}/{}/{}", name, id))
+                    .await
+                    .unwrap()
+                    .bytes_stream();
+
+                while let Some(msg) = stream.next().await {
+                    println!("{:?}", msg);
+                }
+            }
+            Commands::Status { name, id } => {
+                let res = reqwest::get(format!("{ADDR}/{}/{}", name, id))
+                    .await
+                    .unwrap();
+                if res.status().is_success() {
+                    let res1 = res.text().await.unwrap();
+                    let thing: StatusResponse = serde_json::from_str(&res1).unwrap();
+
+                    // Format status into a reply
+                    let s = if let Some(players) = thing.sample.as_ref() {
+                        players
+                            .iter()
+                            .fold(String::from(":\n"), |a, b| format!("{a}\t{}\n", b.name))
+                    } else {
+                        String::from("\n")
+                    };
+                    let out = format!(
+                        "{} [{}] [{}/{}]{}",
+                        id, thing.version, thing.online_players, thing.max_players, s
+                    );
+                    println!("{}", out);
+                } else {
+                    println!("{:?}", res.text().await.unwrap());
+                }
+
+                //println!("Body: {:?}", thing);
+            }
+            Commands::New {
+                name,
+                id,
+                path,
+                port,
+                version,
+                server_type,
+            } => {
+                let new = New {
+                    name: name.clone(),
+                    id: id.clone(),
+                    path: path.clone(),
+                    port: *port,
+                    version: version.clone(),
+                    server_type: server_type.clone(),
+                };
+                println!("Sending...");
+                println!(
+                    "{:?}",
+                    reqwest::Client::new()
+                        .post(format!("{ADDR}/{}", name))
+                        .body(serde_json::to_string(&new).unwrap())
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap()
+                );
+            }
+            Commands::List { name } => {
+                let res = reqwest::get(format!("{ADDR}/{}", name)).await.unwrap();
+                let res_str: ListResponse =
+                    serde_json::from_str(&res.text().await.unwrap()).unwrap();
+                let output_str = res_str
+                    .servers
+                    .iter()
+                    .fold(String::from("Servers:\n"), |a, b| format!("{a}{b}\n"));
+
+                println!("{}", output_str);
+            }
+            Commands::CleanOutput { name, id } => {
+                let mut stream = reqwest::get(format!("{ADDR}/{}/{}", name, id))
+                    .await
+                    .unwrap()
+                    .bytes_stream();
+
+                while let Some(msg) = stream.next().await {
+                    println!("{}", std::str::from_utf8(&msg.unwrap()).unwrap());
+                }
+            }
         }
-        Ok(Box::new(Start { name: args[1].clone(), id: args[2].clone() }))
     }
-
-    async fn execute(&self) {
-        println!("{:?}", reqwest::Client::new().put(format!("{ADDR}/{}/{}", self.name, self.id)).send().await.unwrap());
-    }
-
-}
-
-struct Stop {
-    name: String,
-    id: String,
-}
-
-#[async_trait]
-impl Command for Stop {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 3 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-        Ok(Box::new(Stop { name: args[1].clone(), id: args[2].clone() }))
-    }
-
-    async fn execute(&self) {
-        println!("{:?}", reqwest::Client::new().put(format!("{ADDR}/{}/{}", self.name, self.id)).send().await.unwrap());
-    }
-}
-
-struct Exec {
-    name: String,
-    id: String,
-    cmd: Vec<String>,
-}
-
-#[async_trait]
-impl Command for Exec {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 4 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-        Ok(Box::new(Exec { name: args[1].clone(), id: args[2].clone(), cmd: args[3..].to_vec() }))
-    }
-
-    async fn execute(&self) {
-        let body = format!("{{\"args\":[{}]}}",self.cmd.iter().fold(String::new(), |a, b| format!("{a} \"{b}\",") ).trim().trim_end_matches(","));
-        println!("exec {body}");
-        println!("{:?}", reqwest::Client::new().post(format!("{ADDR}/{}/{}", self.name, self.id))
-            .body(body)
-            .send().await);
-    }
-}
-
-struct Output {
-    name: String,
-    id: String,
-}
-
-#[async_trait]
-impl Command for Output {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 3 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-        Ok(Box::new(Output { name: args[1].clone(), id: args[2].clone() }))
-    }
-
-    async fn execute(&self) {
-        let mut stream = reqwest::get(format!("{ADDR}/{}/{}", self.name, self.id)).await.unwrap().bytes_stream();
-
-        while let Some(msg) = stream.next().await {
-            println!("{:?}", msg);
-        }
-    }
-}
-
-struct Status {
-    name: String,
-    id: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -115,33 +277,9 @@ struct Player {
     id: String,
 }
 
-#[async_trait]
-impl Command for Status {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 3 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-        Ok(Box::new(Status { name: args[1].clone(), id: args[2].clone() }))
-    }
-
-    async fn execute(&self) {
-        let res = reqwest::get(format!("{ADDR}/{}/{}", self.name, self.id)).await.unwrap();
-        if res.status().is_success() {
-            let res1 = res.text().await.unwrap();
-            let thing: StatusResponse = serde_json::from_str(&res1).unwrap();
-
-            // Format status into a reply
-            let s = if let Some(players) = thing.sample.as_ref() {
-                players.iter().fold(String::from(":\n"), |a, b| format!("{a}\t{}\n", b.name))
-            } else { String::from("\n") };
-            let out = format!("{} [{}] [{}/{}]{}", self.id, thing.version, thing.online_players, thing.max_players, s);
-            println!("{}", out);
-        } else {
-            println!("{:?}", res.text().await.unwrap());
-        }
-
-        //println!("Body: {:?}", thing);
-    }
+#[derive(Deserialize)]
+struct ListResponse {
+    servers: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -152,109 +290,4 @@ struct New {
     port: Option<u16>,
     version: Option<String>,
     server_type: Option<String>,
-}
-
-#[async_trait]
-impl Command for New {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 3 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-
-        let port = if let Some(string) = find_flag("p", &args) {
-            Some(string.parse::<u16>().unwrap()) 
-        } else {
-            None
-        };
-
-        // REDO this so that we can do flags
-        Ok(Box::new(New { 
-            name: args[1].clone(),
-            id: args[2].clone(),
-            path: find_flag("d", &args),
-            port,
-            version: find_flag("v", &args),
-            server_type: find_flag("t", &args),
-        }))
-    }
-
-    async fn execute(&self) {
-        println!("Sending...");
-        println!("{:?}", reqwest::Client::new().post(format!("{ADDR}/{}", self.name))
-            .body(serde_json::to_string(self).unwrap())
-            .send().await.unwrap().text().await.unwrap());
-    }
-}
-
-fn find_flag(flag: &str, args: &Vec<String>) -> Option<String> {
-    if let Some(pos) = args.iter().position(|a| *a == format!("-{flag}")) {
-        Some(args[pos+1].clone())
-    } else {
-        None
-    }
-}
-
-struct List {
-    name: String,
-}
-
-#[derive(Deserialize)]
-struct ListResponse {
-    servers: Vec<String>,
-}
-
-#[async_trait]
-impl Command for List {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 2 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-
-        Ok(Box::new(List { name: args[1].clone() }))
-    }
-
-    async fn execute(&self) {
-        let res = reqwest::get(format!("{ADDR}/{}", self.name)).await.unwrap();
-        let res_str: ListResponse = serde_json::from_str(&res.text().await.unwrap()).unwrap();
-        let output_str = res_str.servers.iter().fold(String::from("Servers:\n"), |a, b| { format!("{a}{b}\n") });
-        
-        println!("{}", output_str);
-    }
-}
-
-struct CleanOutput {
-    name: String,
-    id: String,
-}
-
-#[async_trait]
-impl Command for CleanOutput {
-    fn build_from_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
-        if args.len() < 3 {
-            return Err::<Box<dyn Command>, String>(String::from("Too few args"));
-        }
-        Ok(Box::new(CleanOutput { name: args[1].clone(), id: args[2].clone() }))
-    }
-
-    async fn execute(&self) {
-        let mut stream = reqwest::get(format!("{ADDR}/{}/{}", self.name, self.id)).await.unwrap().bytes_stream();
-
-        while let Some(msg) = stream.next().await {
-            println!("{}", std::str::from_utf8(&msg.unwrap()).unwrap());
-        }
-    }
-}
-
-pub fn match_command(args: Vec<String>) -> Option<Result<Box<dyn Command>, String>> {
-    match args[1].as_str() {
-        "start" => Some(Start::build_from_args(args)),
-        "stop" => Some(Stop::build_from_args(args)),
-        "exec" => Some(Exec::build_from_args(args)),
-        "fullout" => Some(Output::build_from_args(args)),
-        "status" => Some(Status::build_from_args(args)),
-        "new" => Some(New::build_from_args(args)),
-        "list" => Some(List::build_from_args(args)),
-        "out" => Some(CleanOutput::build_from_args(args)),
-        _ => None,
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-use std::env;
+use mcli::Cli;
+use clap::Parser;
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect(); 
+    let cli = Cli::parse();
 
-    if args.len() < 2 {
-        println!("No command selected");
-    } else {
-        if let Some(cmd) = mcli::match_command(args) {
-            cmd.unwrap().execute().await;
-        } else {
-            println!("Command not found");
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+    }
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(cmd) => {
+            cmd.execute().await;
         }
+        None => {}
     }
 }
